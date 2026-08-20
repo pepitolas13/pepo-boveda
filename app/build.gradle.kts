7e6bc01287bd6d4d90ab3db2e0fc4ab58027ef0f
Daniel Agolli <daniagolli@gmail.com>
2026-08-21 01:20:26 +0200

    Version 0.7-beta: primera beta publica

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.pepotech.pepoboveda"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 7
+        versionName = "0.7-beta"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
